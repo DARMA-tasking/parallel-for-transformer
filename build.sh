c38ab7a7fb5b0db9cb44a8a797c4562ba68e50de
Jonathan Lifflander <jliffla@sandia.gov>
2020-11-17 15:20:07 -0700

    Add module load for ascicgpu19

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+module load sparc-dev/clang-9.0.1_openmpi-4.0.3
+
 source_dir=${1}
 build_dir=${2}
 
@@ -13,5 +15,5 @@ cd "$CH_BUILD"
 
 target=install
 
-cmake -G "${CMAKE_GENERATOR:-Ninja}" "$CH"
+cmake -G "${CMAKE_GENERATOR:-Ninja}" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 "$CH"
 time cmake --build . --target "${target}"
